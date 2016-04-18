5bee07cef0ff7d6b0b11b83cf6894d272d78f9ef
lsetiawan <landungs@uw.edu>
2016-04-18 14:30:07 -0700

    Climatology Ver. 0.1

--- climatology.py
+++ climatology.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
 import os,subprocess,shutil,string
 import gdal
+import rasterio as rio
+from rasterio.warp import calculate_default_transform, reproject, RESAMPLING
+from rasterio import crs
 import numpy as np
 gdal.UseExceptions()
 
-def sort_to_folder(path,months,path1):
+# Sort tif files to designated months
+def sort_to_folder(path,months):
 	print path
 	tif = ".tif"
 	for i in range(0,len(months)):
 		print "Working on " + months[i][1]
 		for files in os.listdir(path):
 			if tif in files and months[i][0] in files and ".aux.xml" not in files:
-				mvpath = os.path.join(path1,months[i][2])
+				mvpath = os.path.join(path,months[i][2])
 				print files
 				shutil.move(files,os.path.join(mvpath,files))
 			else:
 				pass
-			
+
+# Make folders for each month of the year		
 def make_folder(path):
 	os.chdir(path)
 	for month in range(1,13):
@@ -31,71 +36,69 @@ def make_folder(path):
 				print "Creating Directory: " + str(month)
 				os.makedirs(directory)
 
+# Calculate climatology
 def rasterCalc(path,months,climatology,whichProp,userInput):
 	print path
 	if not os.path.exists(climatology):
 		os.makedirs(climatology)
 	
-	data = []
-	arrays = []
-	
 	for i in range(0,len(months)):
 		os.chdir(os.path.join(path,months[i][2]))
 		print os.getcwd()
-
+		data = []
+		arrays = []
 		for files in os.listdir('.'):
 			if ".tif" in files and ".aux.xml" not in files and ".DS_Store" not in files:
-				data.append(gdal.Open(files))
+				data.append(rio.open(files))
 		dataTotal = len(data)
 		print "There are {} files".format(dataTotal)
 		
 		for j in range(0,dataTotal):
-			arrays.append(np.array(data[j].ReadAsArray()))
-		
-		meanArray = np.mean(arrays,axis=0)
-		
+			raw_data = data[j].read(1)
+			arrays.append(np.where(raw_data == -3000, np.nan, raw_data))
+
+		meanArray = np.nanmean(arrays,axis=0)
+		final_array = np.where(meanArray == np.nan, -3000, raw_data)
 		#Get data about original files
-		[cols,rows] = arrays[0].shape
-		trans = data[0].GetGeoTransform()
-		proj = data[0].GetProjection()
-		nodataval = data[0].GetRasterBand(1).GetNoDataValue()
+		cols = data[0].width
+		rows = data[0].height
+		trans = data[0].affine
+		proj = data[0].crs
+		profile = data[0].profile
 		print [cols,rows]
 		print trans
 		print proj
-		print nodataval
-		
-		# Convert Array into GeoTiff
+		print profile
 		outfile = os.path.join(climatology,"{0}_{1}.tif".format(whichProp,months[i][2]))
-		outdriver = gdal.GetDriverByName("GTiff")
-		outdata = outdriver.Create(str(outfile), rows, cols, 1, gdal.GDT_Float32)
-		outdata.GetRasterBand(1).WriteArray(meanArray)
-		if nodataval == None and userInput == "MOD13A3":
-			outdata.GetRasterBand(1).SetNoDataValue(-3000)
-		elif nodataval == None and userInput == "PRISM":
-			outdata.GetRasterBand(1).SetNoDataValue(-9999)
-		else:
-			outdata.GetRasterBand(1).SetNoDataValue(nodataval)
-		outdata.SetGeoTransform(trans)
-		outdata.SetProjection(proj)
-		
-		data = []
-		arrays = []
-		
-	data = None
-	outdata = None
-	
-	if userInput == "MOD13A3":
-		finalClip(climatology)
 
-def finalClip(climatology):
-	os.chdir(climatology)
-	directory = "EVI_Final"
-	if not os.path.exists(directory):
-		os.makedirs(directory)
-	for files in os.listdir('.'):
-		if "EVI_Final" not in files:
-			subprocess.call(['gdal_translate', '-projwin', '-127.829', '49.9184', 
-			'-59.0521', '5.17714', '-of', 'GTiff', files, os.path.join(directory,files)])
+		dst_crs = crs.from_string("EPSG:4326")
+
+		with rio.drivers():
+			profile.update({
+	            'crs': dst_crs,
+	            'transform': trans,
+	            'affine': trans,
+	            'width': cols,
+	            'height': rows
+	        })
+
+			# Convert Array into GeoTiff
+			with rio.open(outfile, 'w', **profile) as dst:
+				dst_array = np.empty((rows, cols), dtype='int16')
+				reproject(
+	                    # Source parameters
+	                    source=final_array,
+	                    src_crs=proj,
+	                    src_transform=trans,
+	                    # Destination paramaters
+	                    destination=dst_array,
+	                    dst_transform=trans,
+	                    dst_crs=dst_crs,
+	                    resampling=RESAMPLING.nearest
+	                    )
+
+				dst.write(dst_array, 1)
+				print "success"
 		
 	
 
@@ -130,12 +133,11 @@ def main():
 			  ["11_01", "November", "11"],
 			  ["12_01", "December", "12"]]
 	if userInput == "MOD13A3":
-		desired_path = "MOD13A3_TIF_epsg4326"
+		desired_path = "MOD13A3.006"
 		whichProp = "EVI"
-		path1 = os.path.join(os.getcwd(),whichProp)
 		climatology = os.path.join(os.getcwd(),"{0}{1}".format(whichProp,"_climatology"))
 		make_folder(os.path.join(os.getcwd(),desired_path))
-		sort_to_folder(os.getcwd(),months,path1)
+		sort_to_folder(os.getcwd(),months)
 		rasterCalc(os.getcwd(),months,climatology,whichProp,userInput)
 	if userInput == "PRISM":
 		os.chdir(os.path.join(os.getcwd(),"PRISM"))
