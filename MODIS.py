36cbeae5f000e6c9941eedeb77faa0eeb96a3a54
lsetiawan <landungs@uw.edu>
2016-05-12 11:34:10 -0700

    update MODIS

--- MODIS.py
+++ MODIS.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
+''' Import the Necessary Libraries '''
 import os
+import pymodis.downmodis as pmod
+import numpy as np
+import gdal
 import subprocess
-import rasterio as rio
-#import pymodis.downmodis as pmod
+from datetime import date, timedelta 
 
+def makeFolder(basepath, foldername):
+    ''' 
+    Function used to create the MOD13A3.006 Folders and for each dates 
 
-# Function to download MODIS tiles from USGS ftp server
-def performDownload(tilelist, foldername, dates):
+    '''
     maindir = "MOD13A3.006"
     if not os.path.exists(maindir):
         print "Creating {}".format("MOD13A3.006")
@@ -16,20 +21,144 @@ def performDownload(tilelist, foldername, dates):
         if not os.path.exists(folder):
             print "Creating {}".format(folder)
             os.makedirs(folder)
-    for i in range(0,len(dates)):
-        if i != len(dates) - 1:
-            print "Working on {0} to {1}".format(dates[i], dates[i + 1])
-            print "Files are saved in {}".format(os.path.join(os.getcwd(), dates[i]))
-            print ""
-            subprocess.call(["modis_download.py", "-r", "-p", "MOD13A3.006", "-t", "{}".format(tilelist), "-f",
-                             "{}".format(dates[i]), "-e", "{}".format(dates[i+1]), "{}".format(os.path.join(os.getcwd(), dates[i]))])
-            """pmod.downModis(os.path.join(os.getcwd(), dates[i]), tiles = tilelist,
-                       product="MOD13A3.006", today=dates[i], enddate=dates[i+1])"""
 
-# Function to create list of tiles to download.
-# In this case, MODIS tiles around lower 48 and central america are downloaded
-# Tiles are based on sinusoidal projection by NASA
+def performDownload(tilelist,dates, i, work_path):
+    '''
+    Downloads MODIS tiles from USGS ftp server and save them to the appropriate
+    date folder 
+    
+    '''
+    print ""
+    print "Working on {0} to {1}".format(dates[i], dates[i+1])
+    print "Files are saved in {}".format(work_path)
+    destination = work_path
+    # make downloader
+    dm = pmod.downModis(destinationFolder = destination,
+                        path="MOLT", product="MOD13A3.006", tiles="{}".format(tilelist),
+                        today="{}".format(dates[i]), enddate="{}".format(dates[i+1]))
+    dm.connect()
+    print "Connection Attempts: " + str(dm.nconnection)
+    # Download all the files in that day
+    print "Please Wait....................."
+    dm.downloadsAllDay()
+    
+            
+
+def makeFileName(x, ext = "tif"):
+    """
+    Generate a new file name by appending a suffix and changing the extension of an input file name 
+    :param x: string, input file name
+    :param suffix: string, suffix to be placed just before file extension (e.g., 'NDVI')
+    :param ext: string, extension. Don't put the period before the extension
+    """
+    base = os.path.splitext(os.path.basename(x))[0]
+    return base + '.' + ext
+
+def processHDF(folder,work_path):
+    '''
+    Perfom processing for the .hdf MODIS Tiles. Reads the data, grab only the desired subdataset,
+    in this case the EVI. Next, GDAL library is used to create numpy array and save the array as 
+    GeoTiff in the appropriate folder.
+    
+    '''
+    if ".tif" not in folder:
+        print "Now converting hdf for {}".format(folder)
+        os.chdir(work_path)
+        print os.getcwd()
+        for hdf in os.listdir('.'):
+            if ".hdf" in hdf and ".txt" not in hdf and ".log" not in hdf and ".xml" not in hdf:
+                print "Working on {0}".format(hdf)
+                sds = gdal.Open(hdf, gdal.GA_ReadOnly).GetSubDatasets()
+                # Read data in arrays
+                # EVI              
+                vi_src = gdal.Open(sds[1][0])
+                vi_np = vi_src.ReadAsArray()
+        
+                # QA
+                QA_src = gdal.Open(sds[10][0])
+                QA_np = QA_src.ReadAsArray()
+        
+        
+			     # Let's take a quick look at the dimension of that first array
+                cols, rows = vi_np.shape
+			     # Perform value replacement and drop QA layer
+                vi_np[np.logical_and(QA_np != 0, QA_np != 1)] = -3000
+			     # De-allocate QA array
+                QA_np = None
+			     # Get Geotransforms and projection of original dataset
+                geoT = vi_src.GetGeoTransform()
+                proj = vi_src.GetProjection()
+        
+			     # Create new dataset to write array to
+                outfile_name = os.path.join(os.getcwd(), makeFileName(hdf, 'tif'))
+                driver = gdal.GetDriverByName('GTiff')
+			     # Create empty dataset using array dimentions
+                dataset = driver.Create(outfile_name, cols, rows, 1, gdal.GDT_Int16)
+                dataset.SetGeoTransform(geoT)
+                dataset.SetProjection(proj)
+                dataset.GetRasterBand(1).SetNoDataValue(-3000)
+                dataset.GetRasterBand(1).WriteArray(vi_np)
+			     # Close datasets and unallocate arrays
+                dataset = None
+                vi_np = None
+                QA_np = None
+                vi_src = None
+                QA_src = None
+
+def reprojectTile(path, end_date, i):
+    '''
+    Reprojection is performed after .hdf is converted to .tif. The tiles are reprojected to 
+    EPSG:4326 by using gdalwarp.
+    
+    '''
+    if not os.path.exists(os.path.join(path,"EPSG436")):       
+        os.makedirs(os.path.join(path,'EPSG4326'))
+        print "Creating folder: EPSG4326"
+        
+    img = []    
+    for tif in os.listdir(path):
+        if ".tif" in tif and ".aux.xml" not in tif and end_date[i+1].strftime("%j") in tif:
+            img.append(tif)
+        if ".hdf" in tif:
+            print "Removing {}".format(tif)
+            os.remove(tif)
+    newpath = os.path.join(path, "EPSG4326")
+    for i in range(0,len(img)):
+        print "Reprojecting {}".format(img[i])
+        subprocess.call(["gdalwarp","-t_srs","EPSG:4326", "-dstnodata",
+        "-3000","{0}".format(img[i]),"{0}".format(os.path.join(newpath, "{0}".format(img[i])))])
+    return newpath
+    
+def mosaicTile(newpath,folder, path, basepath):
+    '''
+    Using gdalmerge.py the EPSG:4326 GeoTiffs are mosaiced together to create a complete map of 
+    the specified date.
+    
+    '''
+    for j in os.listdir(path):
+        if ".tif" in j:
+            print "Removing {}".format(j)
+            os.remove(j)
+    os.chdir(newpath)
+    gdalMerge = ['gdal_merge.py','-ul_lr','-127.829404882663','49.9999999955068',
+    '-59.0520773226612','5.17713995546022','-n', '-3000', '-a_nodata', '-3000','-o','{0}.tif'.format(os.path.join(basepath,folder))]    
+    print "Merging {}".format(folder)    
+    for image in os.listdir('.'):
+        if ".tif" in image and ".aux.xml" not in image and ".DS_Store" not in image:        
+            gdalMerge.append(os.path.join(newpath,image))
+    subprocess.call(gdalMerge)
+	
+	
+
+
 def createTiles():
+    ''' 
+    Function to create list of tiles to download.
+    In this case, MODIS tiles around lower 48 and central america are downloaded
+    Tiles are based on sinusoidal projection by NASA
+    
+    '''
+
     h = []
     v = []
     tile = []
@@ -51,8 +180,11 @@ def createTiles():
     return tile
 
 
-# Function to create list of dates in the format for pyModis to ingest
 def createDates(startyear, endyear):
+    '''
+    Function to create list of dates in the format for pyModis to ingest
+
+    '''
     folderlist = []
     datelist = []
     for year in range(startyear, (endyear + 1)):
@@ -65,16 +197,44 @@ def createDates(startyear, endyear):
                 datelist.append(str(year) + "-" + str(month) + "-0" + str(1))
     return folderlist, datelist
 
+def getDeltas(dates):
+    '''
+    Create list of days between the monthly date 
+
+    '''
+    comma_date = []
+    end_date = []
+    for d in range(0,len(dates)):
+        comma_date.append(dates[d].replace("-",","))
+    for cd in range(0,len(comma_date)):
+        if cd != len(comma_date) - 1:
+            a_split = map(int, comma_date[cd].split(','))
+            a = date(a_split[0],a_split[1],a_split[2])
+            end_date.append(a)
+    
+    return end_date
+    
 
 def main():
     startyear = input("Enter start year: ")
     endyear = input("Enter end year: ")
+    
     foldernames, dates = createDates(startyear, endyear)
-    tilelist = ' '.join(createTiles())
-    print tilelist
-    print foldernames
-    print dates
-    performDownload(tilelist,foldernames,dates)
+    tilelist = ','.join(createTiles())
+    basepath = os.getcwd()
+    end_date = getDeltas(dates)
+    print end_date
+    makeFolder(basepath, foldernames)
+    mod_path = os.getcwd()
+    print mod_path
+    for i in range(0,len(dates)):
+        if i != len(dates) - 1:
+            work_path = os.path.join(mod_path,foldernames[i+1])
+            performDownload(tilelist,dates,i,work_path)
+            processHDF(foldernames[i+1],work_path)
+            new_path= reprojectTile(work_path,end_date, i)
+            mosaicTile(new_path,foldernames[i+1],work_path,mod_path)
+            
 
 if __name__ == '__main__':
     main()
--- climatology.py
+++ climatology.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 import os,subprocess,shutil,string
+import gdal
+import rasterio as rio
+from rasterio.warp import calculate_default_transform, reproject, RESAMPLING
+from rasterio import crs
+import numpy as np
+gdal.UseExceptions()
 
-def sort_to_folder(path,months,path1):
+# Sort tif files to designated months
+def sort_to_folder(path,months):
 	print path
 	tif = ".tif"
 	for i in range(0,len(months)):
 		print "Working on " + months[i][1]
 		for files in os.listdir(path):
 			if tif in files and months[i][0] in files and ".aux.xml" not in files:
-				mvpath = os.path.join(path1,months[i][2])
+				mvpath = os.path.join(path,months[i][2])
 				print files
 				shutil.move(files,os.path.join(mvpath,files))
 			else:
 				pass
-			
+
+# Make folders for each month of the year		
 def make_folder(path):
 	os.chdir(path)
 	for month in range(1,13):
@@ -28,84 +36,75 @@ def make_folder(path):
 				print "Creating Directory: " + str(month)
 				os.makedirs(directory)
 
-def rasterCalc(path,months,alphabet,climatology,whichProp,origin):
+# Calculate climatology
+def rasterCalc(path,months,climatology,whichProp,userInput):
 	print path
-	print months
-	print alphabet
-	calcSUM = " "
+	if not os.path.exists(climatology):
+		os.makedirs(climatology)
+	
 	for i in range(0,len(months)):
 		os.chdir(os.path.join(path,months[i][2]))
 		print os.getcwd()
-		if not os.path.exists(os.path.join(climatology,months[i][1])):
-			os.makedirs(os.path.join(climatology,months[i][1]))
-		#for j in range(0,len(os.listdir('.'))):
-		#	print j
-		Alpha = []
-		addAlpha = []
-		filelist = []
-		addition = []
-		filenum = []
-
+		data = []
+		arrays = []
 		for files in os.listdir('.'):
 			if ".tif" in files and ".aux.xml" not in files and ".DS_Store" not in files:
-				filenum.append(files)
-		print "There are {} files".format(len(filenum))
-		length = len(filenum)
-		if length > 25:
-			for j in range(0,len(filenum),26):
-				for k in range(0,26):
-					if j + k < len(filenum):
-						newfile = "-{0} {1}".format(alphabet[k],filenum[j+k])
-						filelist.append(newfile)
-						Alpha.append("{}".format(alphabet[k]))
+				data.append(rio.open(files))
+		dataTotal = len(data)
+		print "There are {} files".format(dataTotal)
 		
-		for m in range(0,len(filelist),26):
-			addition.append(filelist[m:26+m])
-			addAlpha.append(Alpha[m:26+m])
+		for j in range(0,dataTotal):
+			raw_data = data[j].read(1)
+			arrays.append(np.where(raw_data == -3000, np.nan, raw_data))
+
+		meanArray = np.nanmean(arrays,axis=0)
+		final_array = np.where(meanArray == np.nan, -3000, raw_data)
+		#Get data about original files
+		cols = data[0].width
+		rows = data[0].height
+		trans = data[0].affine
+		proj = data[0].crs
+		profile = data[0].profile
+		print [cols,rows]
+		print trans
+		print proj
+		print profile
+		outfile = os.path.join(climatology,"{0}_{1}.tif".format(whichProp,months[i][2]))
+
+		dst_crs = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,' \
+				  '298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],' \
+				  'PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,' \
+				  'AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]'
+
+		with rio.drivers():
+			profile.update({
+	            'crs': dst_crs,
+	            'transform': trans,
+	            'affine': trans,
+	            'width': cols,
+	            'height': rows
+	        })
+
+			# Convert Array into GeoTiff
+			with rio.open(outfile, 'w', **profile) as dst:
+				dst_array = np.empty((rows, cols), dtype='int16')
+				reproject(
+	                    # Source parameters
+	                    source=final_array,
+	                    src_crs=proj,
+	                    src_transform=trans,
+	                    # Destination paramaters
+	                    destination=dst_array,
+	                    dst_transform=trans,
+	                    dst_crs=dst_crs,
+	                    resampling=RESAMPLING.nearest
+	                    )
+
+				dst.write(dst_array, 1)
+				print "success"
 		
-		for l in range(0,len(addition)):
-			strlist_file = ' '.join(addition[l])
-			strlist_calc = '+'.join(addAlpha[l])
-			calcSUM = "({0})".format(strlist_calc)	
-			print calcSUM
-			print strlist_file
-			print calcSUM
-			os.system("gdal_calc.py {0} {1} {2} {3} {4} {5} {6}".\
-				format(strlist_file,"--outfile={0}{1}_{2}.tif".format(os.path.join(os.path.join(climatology,months[i][1]),"{}_".format(whichProp)),months[i][1],l),
-					"--calc=\"{}\"".format(calcSUM),"--NoDataValue=-3.4028234663852886e+38",
-					"--type=Float32","--overwrite","--NoDataValue=-9999"))
-
-	rasterMean(climatology,months,alphabet,calcSUM,length,whichProp)
-
-def rasterMean(path,months,alphabet,calcSUM,length,whichProp):
-	os.chdir(path)
-	fileArray = []
-	alphaArray = []
-	alphaAdd = []
-	for i in range(0,len(months)):
-		os.chdir(os.path.join(path,months[i][1]))
-		for files in os.listdir('.'):
-			if ".tif" in files and ".aux.xml" not in files and ".DS_Store" not in files:
-				fileArray.append(files)
-		for j in range(0,len(fileArray)):
-			alphaArray.append("-{0} {1}".format(alphabet[j],fileArray[j]))
-			alphaAdd.append("{}".format(alphabet[j]))
-		for l in range(0,len(alphaArray)):
-			strlist_file = ' '.join(alphaArray[l])
-			strlist_calc = '+'.join(alphaAdd[l])
-			calcSUM = "({0})/{1}".format(strlist_calc,length)	
-			print calcSUM
-			print strlist_file
-			print calcSUM
-			os.system("gdal_calc.py {0} {1} {2} {3} {4} {5} {6}".\
-				format(strlist_file,"--outfile={0}{1}_{2}.tif".format(os.path.join(path,"{}_".format(whichProp)),months[i][1],l),
-					"--calc=\"{}\"".format(calcSUM),"--NoDataValue=-3.4028234663852886e+38",
-					"--type=Float32","--overwrite","--NoDataValue=-9999"))
-			
-
-	os.chdir(path)
 	
-	print fileArray
+
 
 ## Function for PRISM
 def goFolder(path,path1,months):
@@ -137,21 +136,20 @@ def main():
 			  ["11_01", "November", "11"],
 			  ["12_01", "December", "12"]]
 	if userInput == "MOD13A3":
-		alphabet = list(string.ascii_uppercase)
-		desired_path = "MOD13A3_TIF_epsg4326"
+		desired_path = "MOD13A3.006"
+		whichProp = "EVI"
+		climatology = os.path.join(os.getcwd(),"{0}{1}".format(whichProp,"_climatology"))
 		make_folder(os.path.join(os.getcwd(),desired_path))
 		sort_to_folder(os.getcwd(),months)
-		rasterCalc(os.getcwd(),months,alphabet)
+		rasterCalc(os.getcwd(),months,climatology,whichProp,userInput)
 	if userInput == "PRISM":
-		alphabet = list(string.ascii_uppercase)
-		origin = os.path.join(os.getcwd(),"PRISM")
 		os.chdir(os.path.join(os.getcwd(),"PRISM"))
 		whichProp = raw_input("Which property would you like? [ppt or tmean]: ")
 		path1 = os.path.join(os.getcwd(),whichProp)
 		climatology = os.path.join(os.getcwd(),"{0}{1}".format(whichProp,"_climatology"))
 		make_folder(os.path.join(path1))
 		goFolder(os.getcwd(),path1,months)
-		#rasterCalc(os.getcwd(),months,alphabet,climatology,whichProp,origin)
+		rasterCalc(os.getcwd(),months,climatology,whichProp,userInput)
 	
 if __name__ == '__main__' :
 	main()
